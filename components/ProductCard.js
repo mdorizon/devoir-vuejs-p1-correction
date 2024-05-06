14a9f93d1fb08fc9b0a58cef45ab78cce6ecd60c
Maxence <mdorizon17@gmail.com>
2024-05-06 12:10:38 +0200

    :sparkles: cart

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -4,16 +4,21 @@ export const ProductCard = {
     price: Number,
     src: String,
     name: String,
+    like: Boolean,
+    number: Number,
+    likeclicked: Function,
+    buyclicked: Function,
   },
   template: `
     <div class="card" :key="id">
       <img :src="src" :alt="name + id">
       <span class="price">{{ price }} €</span>
       <div>
-        <button class="btn btn-primary">
-          <i class="fa-regular fa-heart"></i>
+        <button class="btn btn-primary" @click="$emit('likeclicked', id)">
+          <i class="fa-regular fa-heart" v-if="this.like == false"></i>
+          <i class="fa-solid fa-heart liked" v-if="this.like == true"></i>
         </button>
-        <button class="btn btn-secondary">
+        <button class="btn btn-secondary" @click="$emit('buyclicked', id)">
           <i class="fa-solid fa-cart-shopping"></i>
         </button>
       </div>
--- script.js
+++ script.js
@@ -8,7 +8,59 @@ createApp({
   },
   data(){
     return {
-      itemList: []
+      itemList: [],
+      buyList: [],
+      numberOfLike: 0,
+      totalPrice: 0,
+    }
+  },
+  methods: {
+    likeclicked(id) {
+      const product = this.itemList.find((product) => product.id == id);
+      if (product.like == false) {
+        product.like = true
+        this.numberOfLike++
+      } else {
+        product.like = false
+        this.numberOfLike--
+      }
+    },
+    buyclicked(id) {
+      const product = this.itemList.find((product) => product.id == id);
+      const buyItemExist = this.buyList.find((item) => item.id == id);
+      if (buyItemExist == null) {
+        this.buyList.push({id: product.id});
+      }
+      product.number ++
+      this.initBuyList();
+    },
+    increase(id) {
+      const product = this.itemList.find((product) => product.id == id);
+      product.number ++
+      this.initBuyList();
+    },
+    decrease(id) {
+      const product = this.itemList.find((product) => product.id == id);
+      const itemIndex = this.buyList.findIndex((item) => item.id == id);
+      product.number --
+      if(product.number <= 0) {
+        this.buyList.splice(itemIndex, 1);
+      }
+      this.initBuyList();
+    },
+    removeItem(id) {
+      const product = this.itemList.find((product) => product.id == id);
+      const itemIndex = this.buyList.findIndex((item) => item.id == id);
+      this.buyList.splice(itemIndex, 1);
+      product.number = 0
+      this.initBuyList();
+    },
+    initBuyList() {
+      let price = 0;
+      this.buyList.forEach(item => {
+        price += this.itemList[item.id].price * this.itemList[item.id].number;
+      });
+      this.totalPrice = price;
     }
   },
   mounted(){
